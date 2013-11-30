4e146c07b3adeb064a3dd1fc5064fa5373325a06
Jnr <Jnr@192.168.1.102>
2013-11-30 14:45:45 -0600

    Proyecto limpio, just hello world

---
+++ src/jnr/datatypeextraction/Main.java
@@ -0,0 +1,14 @@
+package jnr.datatypeextraction;
+
+/**
+ *
+ * @author Jnr
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    
+    }
+    
+}
